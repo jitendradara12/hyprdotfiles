83333967b0c0f9931eb34a37dd030fee19f8b8eb
sastauser <jitendradara4464@gmail.com>
2026-05-06 22:21:34 +0530

    postupdate fix

--- hypr/scripts/LockScreen.sh
+++ hypr/scripts/LockScreen.sh
@@ -11,6 +11,5 @@
 hyprlock #just works
 
 # Ensure weather cache is up-to-date before locking (Waybar/lockscreen readers)
-bash "$HOME/.config/hypr/UserScripts/WeatherWrap.sh" >/dev/null 2>&1 &
-
-loginctl lock-session
+#bash "$HOME/.config/hypr/UserScripts/WeatherWrap.sh" >/dev/null 2>&1 &
+#loginctl lock-session
